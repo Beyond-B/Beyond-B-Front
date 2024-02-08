e9ffd29789a5510368075f1e2094472c5078b482
Seoneui <ellieella2031@gmail.com>
2024-02-08 15:17:16 +0900

    [Feat] diary plus btn 추가

--- app/src/main/java/com/example/beyond_b/diary/DiaryFragment.java
+++ app/src/main/java/com/example/beyond_b/diary/DiaryFragment.java
@@ -9,36 +9,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.beyond_b.R;
+import com.example.beyond_b.databinding.FragmentDiaryBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DiaryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DiaryFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FragmentDiaryBinding binding;
 
     public DiaryFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DiaryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DiaryFragment newInstance(String param1, String param2) {
         DiaryFragment fragment = new DiaryFragment();
         Bundle args = new Bundle();
@@ -60,7 +43,17 @@ public class DiaryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false);
+        binding= FragmentDiaryBinding.inflate(inflater, container, false);
+        View rootView = binding.getRoot();
+
+        binding.diaryWriteFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        return rootView;
     }
+
+
 }
