cb266c2587f37af2abbc433300aee09952450ba9
Seoneui <ellieella2031@gmail.com>
2024-02-12 17:12:57 +0900

    로그인 layout 구현

---
+++ app/src/main/java/com/example/beyond_b/sign_up/NewAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.beyond_b.sign_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.beyond_b.R;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
