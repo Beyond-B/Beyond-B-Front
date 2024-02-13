5dbc48edf5ca9c2931c700b16bcc1949e28727aa
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-02-13 09:59:22 +0900

    [Feat] 탭 및 view pager 레이아웃 구현, Adapter 기본 설정

---
+++ app/src/main/java/com/example/beyond_b/book/TabAdapter.java
@@ -0,0 +1,28 @@
+package com.example.beyond_b.book;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabAdapter extends FragmentStateAdapter {
+
+    public TabAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new Fragment();
+        }
+        return new Fragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
