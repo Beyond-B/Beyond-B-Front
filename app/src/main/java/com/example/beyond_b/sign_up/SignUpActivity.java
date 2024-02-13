9bc48a9f7016e3d96f4a47ce6347a647f3402cea
Seoneui <ellieella2031@gmail.com>
2024-02-13 14:32:17 +0900

    icon 적용

--- app/src/main/java/com/example/beyond_b/sign_up/SignUpActivity.java
+++ app/src/main/java/com/example/beyond_b/sign_up/SignUpActivity.java
@@ -2,15 +2,30 @@ package com.example.beyond_b.sign_up;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.beyond_b.R;
+import com.example.beyond_b.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signupNewAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, NewAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
