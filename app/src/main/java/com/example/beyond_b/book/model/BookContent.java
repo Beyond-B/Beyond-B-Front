de25a08ab6654de19bcc220f846680cbe3a56843
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2024-02-19 01:37:23 +0900

    [Add] BookContent.java, BookDetailResult.java 추가
    커밋 누락..

---
+++ app/src/main/java/com/example/beyond_b/book/model/BookContent.java
@@ -0,0 +1,10 @@
+package com.example.beyond_b.book.model;
+
+public class BookContent {
+    private int bookId;
+    private String title;
+    private String bookSummary;
+    private String bookImage;
+    private String author;
+    private String publisher;
+}
---
+++ app/src/main/java/com/example/beyond_b/book/model/BookDetailResult.java
@@ -0,0 +1,8 @@
+package com.example.beyond_b.book.model;
+
+public class BookDetailResult {
+    private BookContent bookContent;
+    private String quiz1Date;
+    private String quiz2Date;
+    private String quiz3Date;
+}
